02a9a8da341024aaf6a73764f1e54c5b1b816b37
Adesh Shah <shah0150@algonquinlive.com>
2023-10-30 14:31:59 -0400

    Initial Commit

---
+++ MVVM1/MVVM1App.swift
@@ -0,0 +1,17 @@
+//
+//  MVVM1App.swift
+//  MVVM1
+//
+//  Created by Adesh Shah on 2023-10-30.
+//
+
+import SwiftUI
+
+@main
+struct MVVM1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
